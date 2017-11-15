99a27542fca739aada10d845b2f60f69ed275230
Lou Huang <lou@louhuang.com>
2017-11-15 18:36:43 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Chat from './Chat'
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <Chat />
+      </div>
+    )
+  }
+}
+
+export default App

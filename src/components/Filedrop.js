b2b4dc9b496668d63b3eb339452f86d9658df1d4
Lou Huang <lou@louhuang.com>
2017-11-23 11:14:04 -0500

    move components to subdir

---
+++ src/components/Filedrop.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class Filedrop extends React.Component {
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    return (
+      <div />
+    )
+  }
+}

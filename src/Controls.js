10085b1f04f48f60d0ba26daa231717bbf37d641
Lou Huang <lou@louhuang.com>
2017-11-23 10:05:31 -0500

    add source sans pro font

--- src/Controls.js
+++ src/Controls.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import './Controls.css'
 
 export default class Controls extends React.Component {
   render () {
-    return <div />
+    return (
+      <div className="controls">
+        <h1>Scriptchat</h1>
+      </div>
+    )
   }
 }

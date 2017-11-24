7f76f30560eef973cd5c1b6e49a3e0ae2cf29ee8
Lou Huang <lou@louhuang.com>
2017-11-24 14:20:03 -0500

    add areas to fill in stuff

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -7,6 +7,27 @@ export default class Controls extends React.Component {
     return (
       <div className="controls">
         <h1>Scriptchat</h1>
+        <div className="properties">
+          <input
+            type="text"
+            name="title"
+            className="input-with-label"
+            id="title"
+            required
+            autocomplete="off"
+          />
+          <label for="title"><span>Title</span></label>
+
+          <input
+            type="text"
+            name="author"
+            className="input-with-label"
+            id="author"
+            required
+            autocomplete="off"
+          />
+          <label for="author"><span>Author</span></label>
+        </div>
         <Filedrop />
       </div>
     )

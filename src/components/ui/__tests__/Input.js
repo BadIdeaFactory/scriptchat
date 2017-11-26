4d3feac9462efb9a0d1299d5c6c716f4dfca93fe
Lou Huang <lou@louhuang.com>
2017-11-26 14:10:42 -0500

    add test for input component

--- src/components/ui/__tests__/Input.js
+++ src/components/ui/__tests__/Input.js
@@ -1,24 +1,36 @@
 /* eslint-env jest */
 import React from 'react'
 import ReactDOM from 'react-dom'
-import enzyme from 'enzyme'
+import { shallow } from 'enzyme'
 import Input from '../Input'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<Input label="foo" />, div)
-})
+describe('Input', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<Input label="foo" />, div)
+  })
 
-it('runs change handler when value is changed', () => {
-  const div = document.createElement('div')
-  const handler = jest.fn()
+  it('runs change handler when value is changed', () => {
+    const value = 'foo'
+    const handler = jest.fn()
+    const wrapper = shallow(<Input label="foo" onChange={handler} />)
+    const input = wrapper.find('input')
 
-  ReactDOM.render(<Input label="foo" onChange={handler} />, div)
+    // Simulates a change event for each keystroke
+    for (let i = 1; i <= value.length; i++) {
+      input.simulate('change', { target: { value: value.substr(0, i) } })
+    }
 
-  expect(handler.mock.calls[0][0]).toBe(0)
-})
+    // The onChange handler should be called for each keystroke
+    expect(handler).toHaveBeenCalledTimes(value.length)
+    expect(handler).toHaveBeenLastCalledWith(value)
+  })
 
-it('is a no-op when change handler is not provided', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<Input label="foo" />, div)
+  it('is a no-op when change handler is not provided', () => {
+    const value = 'foo'
+    const handler = jest.fn()
+    const wrapper = shallow(<Input label="foo" />)
+    wrapper.find('input').simulate('change', { target: { value } })
+    expect(handler).not.toBeCalled()
+  })
 })
---
+++ src/setupTests.js
@@ -0,0 +1,5 @@
+// Automatically loaded by create-react-app at the beginning of test
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+configure({ adapter: new Adapter() })

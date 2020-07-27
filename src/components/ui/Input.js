cabc8973dec0ab436efe16b26fe04d0ea623c413
Lou Huang <lou@louhuang.com>
2020-07-27 10:39:39 -0400

    refactor(Input): port to function component
    The original class component used deprecated `componentWillReceiveProps` to change the
    internal state of the component in response to an incoming prop change. Since the state
    was merely mirroring the prop, I decided that there was no need to have internal state
    at all anymore. In addition, the refactor to a hook, on top of removing internal state,
    simplifies this component dramatically.

--- src/components/ui/Input.js
+++ src/components/ui/Input.js
@@ -2,56 +2,36 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './Input.css'
 
-export default class Input extends React.Component {
-  static propTypes = {
-    label: PropTypes.string.isRequired,
-    value: PropTypes.string,
-    onChange: PropTypes.func
-  }
-
-  static defaultProps = {
-    value: '',
-    onChange: () => {} // no-op
-  }
-
-  constructor (props) {
-    super(props)
-
-    this.id = this.props.label.toLowerCase().replace(' ', '_') + '-' + Date.now()
-
-    this.state = {
-      value: this.props.value
-    }
-  }
-
-  componentWillReceiveProps (nextProps) {
-    if (this.props.value !== nextProps.value) {
-      this.setState({ value: nextProps.value })
-    }
-  }
-
-  onChange = (event) => {
-    const value = event.target.value
-
-    this.setState({ value })
-    this.props.onChange(value)
-  }
-
-  render () {
-    return (
-      <div>
-        <input
-          type="text"
-          className="input-with-label"
-          required
-          autoComplete="off"
-          name={this.props.label}
-          value={this.state.value}
-          onChange={this.onChange}
-          id={this.id}
-        />
-        <label htmlFor={this.id}><span>{this.props.label}</span></label>
-      </div>
-    )
-  }
+Input.propTypes = {
+  label: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  onChange: PropTypes.func
 }
+
+function Input ({ label, value = '', onChange = () => {} }) {
+  // an ID is required so that labels can attach to inputs
+  const id = label.toLowerCase().replace(' ', '_') + '-' + Date.now()
+
+  function handleChange (event) {
+    const value = event.target.value
+    onChange(value)
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        className="input-with-label"
+        required
+        autoComplete="off"
+        name={label}
+        value={value}
+        onChange={handleChange}
+        id={id}
+      />
+      <label htmlFor={id}><span>{label}</span></label>
+    </div>
+  )
+}
+
+export default Input

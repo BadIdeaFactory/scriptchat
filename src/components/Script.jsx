5291203f611b69df9e056250b739ffe2ef9e319e
Lou Huang <lou@louhuang.com>
2020-08-07 15:50:21 -0400

    feat: adjust styling for printing
    Closes #22.
    
    It's not perfect, but at least weird artifacts like the border around the script
    won't be present during printing, and the UI will be hidden. Pagination and margins
    will vary based on how the browser renders things for the printed page.
    
    This required editing the original script.css styling so it's no longer an exact
    copy of the original from Matt Daly's fountain.js demo page. This does simplify a few
    other things -- the <Script /> component no longer needs to manually apply a class
    style based on window width (we just let media queries take care of it) and the CSS
    file will no longer be ignored by Prettier.

--- src/components/Script.jsx
+++ src/components/Script.jsx
@@ -1,33 +1,12 @@
-import React, { useState, useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import React from 'react'
+import { useSelector } from 'react-redux'
 import './Script.css'
 
 function Script (props) {
-  const [size, setSize] = useState('large')
-  const dpi = size === 'large' ? 'dpi100' : 'dpi72'
   const fountain = useSelector((state) => state.script.fountain)
 
-  useEffect(() => {
-    // Run once to set the correct size on mount
-    handleWindowResize()
-
-    window.addEventListener('resize', handleWindowResize)
-
-    return () => {
-      window.removeEventListener('resize', handleWindowResize)
-    }
-  })
-
-  function handleWindowResize (event) {
-    if (window.innerWidth < 1200) {
-      setSize('small')
-    } else {
-      setSize('large')
-    }
-  }
-
   return (
-    <div id="script" className={`us-letter ${dpi}`}>
+    <div id="script" className="us-letter">
       {(fountain?.html?.title_page) ? (
         <div
           className="page title-page"
